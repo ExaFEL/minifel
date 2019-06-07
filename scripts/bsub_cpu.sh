01571b2158e65283be55c0ac8f1ad0924c96816b
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-06-07 19:42:19 -0400

    Store CuPy cache in burst buffer on Summit.

--- scripts/bsub_cpu.sh
+++ scripts/bsub_cpu.sh
@@ -2,6 +2,7 @@
 #BSUB -P CHM137
 #BSUB -W 0:30
 #BSUB -nnodes 1
+#BSUB -alloc_flags NVME
 #BSUB -o lsf-%J.out
 #BSUB -e lsf-%J.err
 #BSUB -N
@@ -28,4 +29,9 @@ export no_proxy='localhost,127.0.0.0/8,*.ccs.ornl.gov,*.ncrc.gov'
 
 nodes=$(( ( LSB_MAX_NUM_PROCESSORS - 1 ) / 42 ))
 
-jsrun -n $(( nodes * 2 )) --rs_per_host 2 --tasks_per_rs 1 --cpu_per_rs 21 --gpu_per_rs 3 --bind rs "$root_dir"/scripts/pick_hcas.py legion_python user -ll:py 1 -ll:cpu 1
+# CuPy tries to store its cache in ~/.cupy
+# export HOME=$MEMBERWORK/chm137/run
+# mkdir -p $HOME
+export HOME=/mnt/bb/$USER
+
+time jsrun -n $(( nodes * 2 )) --rs_per_host 2 --tasks_per_rs 1 --cpu_per_rs 21 --gpu_per_rs 1 --bind rs "$root_dir"/scripts/pick_hcas.py legion_python user -ll:py 1 -ll:cpu 1 -level announce=2
