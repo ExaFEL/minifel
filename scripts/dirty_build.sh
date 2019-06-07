60a08efe0697747a39f7abf4330c630bf77ec24e
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-06-06 17:00:13 -0700

    Fix typo.

--- scripts/dirty_build.sh
+++ scripts/dirty_build.sh
@@ -9,5 +9,5 @@ source ../setup/env.sh
 
 mkdir -p build
 cd build
-cmake -DCMAKE_PREFIX_PATH="$LEGION_INSTALL_DIR" ../native_kernels
+cmake -DCMAKE_PREFIX_PATH="$LEGION_INSTALL_DIR" ../../native_kernels
 make -j8
