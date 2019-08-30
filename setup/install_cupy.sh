739ef34e109704d92519de15f65be50f1d4a1394
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-08-30 14:54:36 -0700

    Unlock CuPy version since bug is now fixed.

--- setup/install_cupy.sh
+++ setup/install_cupy.sh
@@ -9,7 +9,7 @@ pushd "$root_dir"
 
 rm -rf cupy
 git clone https://github.com/cupy/cupy.git
-git -C cupy reset --hard 7dca63a17d1f517bcf5595ddcd32a0342d6a95b5
+# git -C cupy reset --hard 7dca63a17d1f517bcf5595ddcd32a0342d6a95b5 # https://github.com/cupy/cupy/issues/2433
 
 conda install -y fastrlock
 
